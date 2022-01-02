ad355e1541731d1b1c1e6d306d24f99d4c9f146f
jalil.nourmohammadi <jalil.nourmohammadi@elka.pw.edu.pl>
2022-01-02 23:56:39 +0100

    add first example on git hub

---
+++ chapter 1/example 1-1.py
@@ -0,0 +1,29 @@
+import collections
+from random import  choice
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range (2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self) -> None:
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+        
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+beer_card = Card('7', 'diamonds')
+
+print(beer_card)
+
+deck = FrenchDeck()
+
+print(len(deck))
+
+print(deck[42])
+
+print (choice(deck))
