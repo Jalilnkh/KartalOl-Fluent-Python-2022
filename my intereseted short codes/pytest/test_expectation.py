fdc3c0533ee5894f4da3787a23113d31c37d0666
jalil.nourmohammadi <jalil.nourmohammadi@elka.pw.edu.pl>
2022-02-04 16:42:46 +0100

    pyteest is done!

--- my intereseted short codes/pytest/test_expectation.py
+++ my intereseted short codes/pytest/test_expectation.py
@@ -1,6 +1,8 @@
 import pytest
 
 
-@pytest.mark.parametrize("test_input, expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+@pytest.mark.parametrize(
+    "test_input, expected", [("3+5", 8), ("2+4", 6), pytest.param("6*9", 42, marks=pytest.mark.xfail)],
+)
 def test_eval(test_input, expected):
     assert eval(test_input) == expected
