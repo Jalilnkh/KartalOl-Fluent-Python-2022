e9e86ec538e8d674ede3753f6a170e77387fe339
Jalilnkh <Jalil.Nourmohammadi@elka.pw.edu.pl>
2023-01-08 15:32:30 +0100

    add some code

--- welcome.py
+++ welcome.py
@@ -1,2 +1,8 @@
 # Welcome to our public code in python 
 print("Welcome to our publicly avialble code for Fluent Python 2022 book")
+while True:
+    a = input("enter a nkey = ")
+
+    if a == 'f':
+        break
+    
