74bda1d57c66adf86f02b7e5bddf17f1a9749871
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-01-19 23:18:21 +0100

    complete chapter 14

--- chapter 14-Inheritance: For Better or For Worse/person.py
+++ chapter 14-Inheritance: For Better or For Worse/person.py
@@ -126,3 +126,36 @@ class Leaf(A, B):
     def ping(self) -> None:
         print(f'{self}.ping() in Leaf')
         super().ping()
+
+def _upper(key):
+    try:
+        return key.upper()
+    except AttributeError:
+        return key
+
+class UpperCaseMixin:
+    def __setitem__(self, key, item):
+        super().__setitem__(_upper(key), item)
+
+    def __getitem__(self, key):
+        return super().__getitem__(_upper(key))
+    
+    def get(self, key, default=None):
+        return super().get(_upper(key), default)
+    
+    def __contains__(self, key):
+        return super().__contains__(_upper(key))
+
+
+class UpperDict(UpperCaseMixin, collections.UserDict):
+    pass
+
+
+class UpperCounter(UpperCaseMixin, collections.Counter):
+    """Specialized 'Counter' that uppercases string keys"""
+
+
+class ThreadingHTTPServer(
+    socketserver.ThreadingMixIn,
+    HTTPServer):
+    daemon_threads = True
