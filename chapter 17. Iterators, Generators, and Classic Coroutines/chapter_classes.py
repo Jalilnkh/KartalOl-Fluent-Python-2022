529d3c4127988b3738d642b59e8a4f7e86f8ab5b
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-01-29 23:58:43 +0100

    work on iterator

--- chapter 17. Iterators, Generators, and Classic Coroutines/chapter_classes.py
+++ chapter 17. Iterators, Generators, and Classic Coroutines/chapter_classes.py
@@ -24,3 +24,18 @@ class Spam:
     
     def __iter__(self):
         pass
+
+
+class Iterator(Iterable):
+    __slots__ = ()
+    @abstractmethod
+    def __next__(self):
+        'Return the next item from the iterator. When exhausted, raise StopIteration'
+        raise StopIteration
+    def __iter__(self):
+        return self
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Iterator:
+            return _check_methods(C, '__iter__', '__next__')
+        return NotImplemented
